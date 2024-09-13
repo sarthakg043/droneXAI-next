6bc177019e03eedbf2cb96b5881f3feaaf921d1b
Sarthak Gupta <sarthakg4953@gmail.com>
2024-09-13 20:28:17 +0530

    logoticker animation done

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import AmazonLogo from "@/assets/images/Amazon-Logo.png"
 import GoogleLogo from '@/assets/images/Google-logo.png'
@@ -6,6 +7,7 @@ import SalesforceLogo from '@/assets/images/Salesforce-Logo.png'
 import SamsungLogo from '@/assets/images/Samsung-Logo-2-500x281.png'
 import AppleLogo from '@/assets/images/Apple-Logo.png'
 import dynamic from 'next/dynamic';
+import { motion } from 'framer-motion';
 
 const DynamicImage = dynamic(() => import('next/image'), { ssr: false });
 
@@ -46,13 +48,25 @@ const LogoTicker: React.FC<LogoTickerProps> = ({id}) => {
         <div className="contaier">
             <h2 className='text-xl text-center text-white/70'>Trusted by the world&#39;s most innovative teams</h2>
             <div 
-                className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 sm:before:w-10 after:w-5 sm:after:w-10 relative after:right-0 before:left-0 before:top-0 after:top-0 before:z-10 after:z-10 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))] "
+                className="flex overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 sm:before:w-10 after:w-5 sm:after:w-10 relative after:right-0 before:left-0 before:top-0 after:top-0 before:z-10 after:z-10 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))] "
             >
-                <div className='flex items-center gap-16'>
+                {/* Infinite scroll */}
+                <motion.div className='flex flex-none items-center gap-16 pr-16'
+                    transition={{ 
+                        duration: 10, 
+                        repeat: Infinity,
+                        ease: 'linear'
+                    }}
+                    initial={{ translateX: 0 }}
+                    animate={{ translateX: '-50%' }}
+                >
                     {images.map(({src, alt}, index) => (
                         <DynamicImage key={index} src={src} alt={alt} className={`w-auto h-20 flex-none filter grayscale ${(alt === "Amazon")? 'brightmess-150' : 'brightness-50'}`} />
                     ))}
-                </div>
+                    {images.map(({src, alt}, index) => (
+                        <DynamicImage key={index} src={src} alt={alt} className={`w-auto h-20 flex-none filter grayscale ${(alt === "Amazon")? 'brightmess-150' : 'brightness-50'}`} />
+                    ))}
+                </motion.div>
             </div>
         </div>
     </div>
